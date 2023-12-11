4a566b8c26427c8d8412a8d6229c4cbb33daa056
CarolinaH <carolinaheredialozano@gmail.com>
2023-12-11 18:20:28 -0500

    Added the SpringBootApplication annotation to the main.

--- src/main/java/com/carobackendhere/PersonalException.java
+++ src/main/java/com/carobackendhere/PersonalException.java
@@ -1,5 +1,8 @@
 package com.carobackendhere;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PersonalException extends Exception{
 
     public PersonalException(String msg){ super(msg);}
