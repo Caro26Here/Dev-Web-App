2b462cf59931595f35dd01f64d23900997351a31
CarolinaH <carolinaheredialozano@gmail.com>
2023-12-11 19:21:24 -0500

    Removed an annotation

--- src/main/java/com/carobackendhere/PersonalException.java
+++ src/main/java/com/carobackendhere/PersonalException.java
@@ -2,7 +2,7 @@ package com.carobackendhere;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class PersonalException extends Exception{
 
     public PersonalException(String msg){ super(msg);}
